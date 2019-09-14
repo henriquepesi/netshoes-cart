61dff19f46c9d240fd24291d8184e51b933b9f8c
henrique <henrique.pedasi@gmail.com>
2019-09-13 23:59:51 -0300

    json server has been set up and images added to json file

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,46 +1,34 @@
-import React from 'react';
+import React, { Component } from 'react';
+import api from '../../services/api';
 
 import { ProductList, Value } from './styles';
 
-export default function Home() {
-  // const [products, setProducts] = useState([]);
+export default class Home extends Component {
+  state = {
+    products: [],
+  };
 
-  // function handleAddTech() {
-  //   setProducts([...products, 'product-1']);
-  // }
+  async componentDidMount() {
+    const response = await api.get('products');
 
-  return (
-    <ProductList>
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/93500751/camisa-do-corinthians-i-2019-nike-feminina-img.jpg"
-          alt="Camiseta Corinthians"
-        />
-        <strong>Camisa Nike Corinthians I</strong>
-        <Value price>R$ 229.9</Value>
-        <Value>3 x de {(229.9 / 3).toFixed(2)}</Value>
-        <button type="submit">Comprar</button>
-      </li>
-      <li>
-        <img
-          src="https://static3.tcdn.com.br/img/img_prod/311840/camiseta_corinthians_raglan_preto_53714_1_20181123123430.jpg"
-          alt="Camiseta Corinthians"
-        />
-        <strong>Camisa Nike Corinthians I</strong>
-        <Value price>R$ 229.9</Value>
-        <Value>3 x de {(229.9 / 3).toFixed(2)}</Value>
-        <button type="submit">Comprar</button>
-      </li>
-      <li>
-        <img
-          src="https://imgcentauro-a.akamaihd.net/900x900/91189631/camiseta-do-corinthians-basic-tr-masculina-img.jpg"
-          alt="Camiseta Corinthians"
-        />
-        <strong>Camisa Nike Corinthians I</strong>
-        <Value price>R$ 229.9</Value>
-        <Value>3 x de {(229.9 / 3).toFixed(2)}</Value>
-        <button type="submit">Comprar</button>
-      </li>
-    </ProductList>
-  );
+    this.setState({ products: response.data });
+  }
+
+  render() {
+    const { products } = this.state;
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={product.image}  alt={product.title}/>
+            <strong>{product.title}</strong>
+            <Value price>{product.currencyFormat} {product.price}</Value>
+            <Value>3 x de {(product.price / 3).toFixed(2)}</Value>
+            <button type="submit">Comprar</button>
+          </li>
+        ))}
+      </ProductList>
+    );
+  }
 }
